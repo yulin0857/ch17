0dc351bf515938c22daacd2d6ce8b375d8374fce
Yulin <hboy15913@yahoo.com>
2021-06-30 13:34:33 +0800

    17-2

--- Generics.kt
+++ Generics.kt
@@ -1,3 +1,12 @@
 class LootBox<T>(item: T) {
     private var loot:T =item
 }
+
+class Fedora(val name: String, val value:Int)
+
+class Coin(val value: Int)
+
+fun main(arg: Array<String>){
+    val lootBoxOne: LootBox<Fedora> = LootBox(Fedora("a generic-looking fedora",15))
+    val lootBoxTwo: LootBox<Coin> = LootBox(Coin(15))
+}
