4b5aadb89bf2430272be4dbffc8564b5104d4b12
Yulin <hboy15913@yahoo.com>
2021-06-30 13:47:42 +0800

    17-5

--- Generics.kt
+++ Generics.kt
@@ -15,6 +15,7 @@ fun main(arg: Array<String>){
     val lootBoxOne: LootBox<Fedora> = LootBox(Fedora("a generic-looking fedora",15))
     val lootBoxTwo: LootBox<Coin> = LootBox(Coin(15))
 
+    lootBoxOne.open = true
     lootBoxOne.fetch()?.run {
         println("You retrieve $name from the box!")
     }
