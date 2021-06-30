3d660899de877bdeb72459a7ad57a22558837678
Yulin <hboy15913@yahoo.com>
2021-06-30 13:49:52 +0800

    17-6

--- Generics.kt
+++ Generics.kt
@@ -5,6 +5,9 @@ class LootBox<T>(item: T) {
     fun fetch(): T?{
         return loot.takeIf { open }
     }
+    fun <R> fetch(lootModFunction: (T) ->R): R?{
+        return lootModFunction(loot).takeIf { open }
+    }
 }
 
 class Fedora(val name: String, val value:Int)
