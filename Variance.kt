5e4a70218578335d325c5f0570b2eb124da5f083
Yulin <hboy15913@yahoo.com>
2021-06-30 14:46:24 +0800

    17-16

---
+++ Variance.kt
@@ -0,0 +1,7 @@
+class Barrel<T>(var item: T) {
+}
+
+fun main(arg: Array<String>){
+    var fadoraBarrel: Barrel<Fedora> = Barrel(Fedora("a generic-looking fedora", 15))
+    var lootBarrel: Barrel<Loot> = Barrel(Coin(15))
+}
